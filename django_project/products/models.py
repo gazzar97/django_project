f92d0ac90047b1536da800044a3182d83f711166
Mahmoud Elgazzar <Mahmoud.Elgazzar@itworx.com>
2022-06-21 19:03:22 +0200

    add models defintions

--- django_project/products/models.py
+++ django_project/products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Product(models.Model):
+    Name = models.CharField(max_length=50)
+    Description = models.CharField(max_length=100)
+    Image = models.CharField(max_length=200)
+
+
+class User(models.Model):
+    Name = models.CharField(max_length=50)
+    Email = models.EmailField()
+    Password = models.CharField(max_length=20)
